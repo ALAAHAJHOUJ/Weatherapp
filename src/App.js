1a516b2f9c023e7170b3a7c991179a7a5129bb0d
hajhouj <alaa.spread@gmail.com>
2025-07-17 02:57:23 +0100

    changement dans le projet

--- src/App.js
+++ src/App.js
@@ -8,14 +8,20 @@ import { motion } from "framer-motion";
 import './fichier.css';
 
 function App() {
+  const pays = [
+  {label:"maroc"},
+  {label:"magfgfc"},
+  {label:"matarec"},
+  {label:"mahfyteferoc"},
+  {label:"mgdtdfsoc"},
+  {label:"mgdfvcoc"},
+  {label:"margdfdoc"},
+];
+
   const [hauteur,setHauteur]=useState("130px");
-  const [selectedFramework, setSelectedFramework] =useState(null);
+  const [selectedFramework, setSelectedFramework] =useState(pays);
   const [loading,setLoading]=useState(true);
 
-
-  const pays = [
-
-];
 
   const RoundedTextField = styled(TextField)({
   '& .MuiOutlinedInput-root': {
@@ -31,18 +37,21 @@ function App() {
 
   },[])
 
+
+
   return (
     <div className="element App border-[1px] w-full min-h-[100vh] bg-[#2d2d69] flex justify-center items-center">
+      
       {loading==true?<motion.div className="w-full h-[100px] flex justify-center text-white" animate={{opacity:[1,0,1]}} transition={{duration:1,repeat:Infinity}}><FaLocationDot className="scale-[3] text-white"></FaLocationDot></motion.div>:
-      <div className={"element2 min-[0px]:shrink-0 min-[360px]:shrink-[1] min-[0px]:w-[100%] min-[360px]:w-[430px] min-w-[353px]  bg-white rounded-[13px] flex items-start flex-wrap transition-[height] duration-[2s] "} style={{boxShadow:"-1px 1px 47px -6px rgba(255,255,255,0.75)",height:`${hauteur}`}}>
+      <div className={"element2 min-[0px]:shrink-0 min-[360px]:shrink-[1] min-[0px]:w-[100%] min-[360px]:w-[430px] min-w-[353px]   bg-white rounded-[13px] flex  flex-wrap transition-[height] duration-[2s] content-start "} style={{boxShadow:"-1px 1px 47px -6px rgba(255,255,255,0.75)",height:`${hauteur}`}}>
             <div className=" h-[130px] w-[16%] flex justify-center items-center">
               <FaLocationDot className="scale-[2.5] translate-x-[-5px] hover:text-gray-400 cursor-pointer" title="Votre position"></FaLocationDot>
             </div>
-            <div className=" h-[130px] w-[70%] flex justify-center items-center">
+            <div className=" h-[130px] w-[70%] flex justify-center items-center ">
                     <Autocomplete
-                      options={pays}
-                      getOptionLabel={(option) => option.label}
-                      onChange={(event, newValue) => setSelectedFramework(newValue)}
+                      options={selectedFramework}
+                      getOptionLabel={(option) => option?.label ? option.label : ''}
+                      onChange={(event, newValue) =>{ console.log(newValue);setSelectedFramework(newValue)}}
                       sx={{ width: '100%'}}
                       renderInput={(params) => (
                         <RoundedTextField {...params} label="Choisissez un pays" variant="outlined"  />
@@ -53,12 +62,12 @@ function App() {
               <IoSearchCircle className="scale-[3.4] hover:text-gray-400 cursor-pointer" title="chercher" onClick={  ()=>{setHauteur((prev)=>{if(prev=="130px") return "580px";else return "130px";})}  }></IoSearchCircle>
             </div>
             {hauteur=="580px"? 
-            <div className="w-full flex flex-wrap content-start h-[400px] border-black border-[1px]">
+            <div className="w-full flex flex-wrap content-start h-[400px] border-black border-[1px] ">
               <div className="w-full flex justify-center mb-5 bg-red-400">
                   <div className="degre border-[1px] border-black w-[150px] h-[150px]">image erreur ou resultat degre</div>
               </div>
               <div className="w-full h-[100px] border-[1px] border-black flex justify-center bg-green-500 mb-5">
-                <div className="border-[1px] border-black w-[200px] h-full">message err</div>
+                <div className="border-[1px] border-black w-[200px] h-full">message err ou pression</div>
               </div>
 
               <div className="w-full h-[90px] border-black border-[1px] bg-yellow-400 flex justify-between">
